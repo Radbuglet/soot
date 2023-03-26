bf3777423665f43bea982c04ff9c57b3453a03a1
Radbuglet <10578060+Radbuglet@users.noreply.github.com>
2023-03-25 17:08:21 -0700

    Fix potential unsoundness with trailing closures

--- README.md
+++ README.md
@@ -1,39 +1,3 @@
 # Soot
 
 **S**elf **O**bject **o**f **T**error - A library to create self-referential objects without unsafe blocks or boilerplate.
-
-```rust
-use soot::soot;
-use std::{
-    cell::RefCell,
-    iter::Cloned,
-    pin::pin,
-    rc::Rc,
-    slice,
-};
-
-fn make_thing<T: Clone>(v: &Rc<RefCell<[T]>>) -> soot![type (&'a mut [T], Cloned<slice::IterMut<'a, T>>); '_] {
-    soot! {
-        do {
-            let mut borrowed = v.borrow_mut();
-            let (left, right) = borrowed.split_at_mut(3);
-        }
-        => (left, right.iter().cloned())
-    }
-}
-
-fn main() {
-    let target = Rc::new(RefCell::new([3, 2, 1, 4, 5]));
-    let (left_slice, iter) = pin!(make_thing()).get();
-    
-    println!("Left slice: {left_slice:?}");
-
-    for value in iter {
-        println!("{value}");
-    }
-}
-```
-
-## Introduction
-
-==TODO==
--- src/lib.rs
+++ src/lib.rs
@@ -178,14 +178,31 @@ pub mod macro_internals {
 #[macro_export]
 macro_rules! self_ref {
     (use $provided:ident in { $($rest:tt)* }) => {
-		$crate::macro_internals::SelfRef::new(|__self_ref_internal_provider| async move {
-			__self_ref_internal_provider.check_covariance(|_, x| x);
+		$crate::macro_internals::SelfRef::new(|__self_ref_internal_and_very_unsafe_provider| async move {
+			__self_ref_internal_and_very_unsafe_provider.check_covariance(|_, x| x);
 
 			$($rest)*
 
+			// This block protects us from trailing closures.
+			{};
+
 			unsafe {
-				// Safety: TODO
-				__self_ref_internal_provider.provide($provided).await;
+				// Safety:
+				//
+				// - We are guaranteed to be executing in an appropriate context because this `async`
+				//   block is given directly to `SelfRef` and all token trees are balanced so `$rest`
+				//   could not have moved us into a different async block. We know that this is the
+				//   appropriate `__self_ref_internal_and_very_unsafe_provider` with the appropriate
+				//   type for the closure because no one else could have bound to this variable
+				//   without being particularly malicious, at which point we can't help them.
+				//
+				// - We've already proven that `T` is covariant through `check_covariance`.
+				//
+				// - Because we're executing in the main `async` block and `provide` is inherently
+				//   blocking, we know that the entire future will be blocked as long as we provide
+				//   `$provided` to the caller.
+				//
+				__self_ref_internal_and_very_unsafe_provider.provide($provided).await;
 			}
 		})
 	};
